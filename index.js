93c0feb99933afa3c90153a6d76f02ced9d5ba1b
Animesh Nilawar <148982099+AnimeshNilawar@users.noreply.github.com>
2025-02-23 22:31:55 +0530

    Add user authentication with signup and login functionality and user can see only their short Urls analytics

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require("express");
 const { connectMongoDB } = require("./connection");
 const URL = require('./models/url');
 const path = require('path');
-const staticRouter = require('./routes/staticRouter')
-const urlRoute = require("./routes/url")
+const cookieParser = require('cookie-parser');
+const { restrictToLoggedinUserOnly, checkAuth } = require('./middlewares/auth')
+
+const staticRouter = require('./routes/staticRouter');
+const urlRoute = require("./routes/url");
+const userRoute = require("./routes/user");
 
 const app = express();
 const PORT = 8001;
@@ -15,9 +19,11 @@ app.set("views", path.resolve("./views"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
-app.use("/url", urlRoute);
-app.use("/", staticRouter);
+app.use("/url", restrictToLoggedinUserOnly, urlRoute);
+app.use("/user", userRoute);
+app.use("/", checkAuth, staticRouter);
 
 app.get("/url/:shortId", async (req, res) => {
     const shortId = req.params.shortId;
--- models/url.js
+++ models/url.js
@@ -11,6 +11,10 @@ const UrlSchema = new mongoose.Schema({
         require: true,
     },
     visitHistory: [{ timestamp: { type: Number } }],
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "users",
+    }
 }, { timestamps: true }
 );
 
